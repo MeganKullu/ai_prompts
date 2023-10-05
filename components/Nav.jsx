f63a166e1135112141db2e17a7b7c941749a0924
MeganKullu <kullumegan@gmail.com>
2023-10-05 12:51:56 +0300

    resolved google OAuth provider error

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,21 +6,22 @@ import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
+
 const Nav = () => {
 
-    const isUserLoggedIn = true;
+    const { data: session} = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDropDown, setToggleDropDown] = useState(false);
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
 
             setProviders(response);
         }
 
-        setProviders();
+        setUpProviders();
     }, [])
 
     return (
@@ -38,7 +39,7 @@ const Nav = () => {
 
             {/* Desktop Navigation */}
             <div className='sm:flex hidden'>
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className='flex gapp-3 md:gap-5'>
                         <Link href='/create-prompt'
                             className='black_btn'>
@@ -49,7 +50,7 @@ const Nav = () => {
 
                         <Link href="/profile">
                             <Image
-                                src="/assets/images/logo.svg"
+                                src={session?.user.image}
                                 width={37}
                                 height={37}
                                 className='rounded-full'
@@ -77,10 +78,10 @@ const Nav = () => {
 
             {/* Mobile Navigation */}
             <div className='sm:hidden flex relative'>
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className='flex'>
                         <Image
-                            src="/assets/images/logo.svg"
+                            src={session?.user.image}
                             width={37}
                             height={37}
                             className='rounded-full'
